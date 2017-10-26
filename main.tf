dbf6853122804d650291bb5097b978a3014bf2bd
Ivan Luckyanov <menzoberronzan58@gmail.com>
2017-10-26 10:48:27 +0300

    Added aws iam role creation for lambda function

--- main.tf
+++ main.tf
@@ -73,6 +73,30 @@ data "archive_file" "lambda" {
   output_path = "app.zip"
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-vpc-execution-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+    role       = "${aws_iam_role.lambda_role.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_lambda_function" "test_lambda" {
   filename         = "app.zip"
   function_name    = "AWSLambdaExecutionCounter"
